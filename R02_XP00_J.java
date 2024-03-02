f667915f88635da974a063cc4c2f32aaccb01696
emmakozl <147213472+emmakozl@users.noreply.github.com>
2024-03-02 15:13:33 -0500

    Update R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -4,6 +4,8 @@ public class R02_XP00_J {
  
   		File someFile = new File("someFileName.txt");
   		// Do something with someFile
-  		someFile.delete();
+  		if (!someFile.delete()) {
+    			// Handle failure to delete the file
+  		}
 	}
 }
