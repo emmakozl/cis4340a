b6b4c59c64e52738a32becbdc545e2ae07ee90b5
emmakozl <147213472+emmakozl@users.noreply.github.com>
2024-03-02 15:05:50 -0500

    Update R00_IDS03_J.java back to problem code

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -3,13 +3,9 @@ public class R00_IDS03_J {
 	public static void main(String[] args){
 
 		if (loginSuccessful) {
-  			logger.severe("User login succeeded for: " + sanitizeUser(username));
+  			logger.severe("User login succeeded for: " + username);
 		} else {
-  			logger.severe("User login failed for: " + sanitizeUser(username));
+  			logger.severe("User login failed for: " + username);
 		}
-	}
-
-	public String sanitizeUser(String username) {
-  		return Pattern.matches("[A-Za-z0-9_]+", username)) ? username : "unauthorized user";
 	}
 }
