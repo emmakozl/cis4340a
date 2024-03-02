84c1ff9bb0535cfea30e6b223290814c5ac78807
emmakozl <147213472+emmakozl@users.noreply.github.com>
2024-03-02 15:53:30 -0500

    Add sixth problem file

---
+++ R07_ERR04_J.java
@@ -0,0 +1,11 @@
+class TryFinally {
+  
+    private static boolean doLogic() {
+  		try {
+  			throw new IllegalStateException();
+  		} finally {
+    		System.out.println("logic done");
+    	return true;
+  		}
+    } 
+}
